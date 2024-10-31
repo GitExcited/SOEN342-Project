ebe6493583c34affac830eec595e8154ebe78557
Yan Pilon <y.pilon9@gmail.com>
2024-10-31 16:26:58 -0400

    created remainder of classes

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,5 +3,8 @@ package main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        //instantiate all classes 
+        // start commandline loop
+        //map 
     }
 }
