7bab28fc0f953d6a7ba6e1530755b6c3fc35f66d
Yan Pilon <y.pilon9@gmail.com>
2024-10-31 18:19:05 -0400

    command-line interface wip

--- src/main/AppSystem.java
+++ src/main/AppSystem.java
@@ -1,5 +1,45 @@
 package main;
 
+enum UserAuthLevel {
+    Client,
+    Instructor,
+    Admin,
+    NotAuthorized
+}
+
 public class AppSystem {
-    
+
+
+    private Admin admin;
+    private Organization organization;
+    private Clients clients;
+    private Instructors instructors;
+    private Bookings bookings;
+    private Offerings offerings;
+    private PublicOfferings publicOfferings;
+
+    private boolean userAuthenticated;
+    private UserAuthLevel authLevel;
+
+
+    public AppSystem(){
+        this.admin = new Admin();
+        this.organization = new Organization();
+        this.clients = new Clients();
+        this.instructors = new Instructors();
+        this.bookings = new Bookings();
+        this.offerings = new Offerings();
+        this.publicOfferings = new PublicOfferings();
+
+        this.userAuthenticated = false;
+        this.authLevel = UserAuthLevel.NotAuthorized;
+    }
+
+    public boolean isUserAuthenticated(){
+        return this.userAuthenticated;
+    }
+
+    public UserAuthLevel getUserAuthLevel(){
+        return this.authLevel;
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,84 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        //instantiate all classes 
-        // start commandline loop
-        //map 
+        AppSystem appSystem = new AppSystem();
+        Scanner in = new Scanner(System.in);
+
+        String userInput = "";
+        boolean running = true;
+        while(running){
+            if (appSystem.isUserAuthenticated()){
+                System.out.println("""
+                        Welcome to the lesson booking system please select an action:
+                        1) Login client
+                        2) Login instructor
+                        3) Login admin
+                        4) Register client
+                        5) Register Instructor
+                        6) View public offerings
+                        """);
+                userInput = in.nextLine();
+                processUserInput(userInput, appSystem.getUserAuthLevel());
+                userInput = "";
+            }else{
+                switch (appSystem.getUserAuthLevel()) {
+                    case Client:
+                    System.out.println("""
+                        Logged in as Client, please choose an action:
+                        1) View public offerings
+                        2) Process Bookings
+                        3) View Bookings
+                        """);
+                    userInput = in.nextLine();
+                    processUserInput(userInput, UserAuthLevel.Client);
+                    userInput = "";
+                        break;
+                    case Instructor:
+                    System.out.println("""
+                        Logged in as Instructor, please choose an action:
+                        1) Take Offerings
+                        2) View taken Offerings
+                        """);
+                        userInput = in.nextLine();
+                        processUserInput(userInput, UserAuthLevel.Instructor);
+                        userInput = "";
+                        break;
+                    case Admin:
+                    System.out.println("""
+                        Logged in as Admin, please choose an action:
+                        1) Create Offering
+                        2) Delete Offering
+                        3) Delete Instructor Account
+                        4) Delete Client Account
+                        """);
+                        userInput = in.nextLine();
+                        processUserInput(userInput, UserAuthLevel.Admin);
+                        userInput = "";
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        in.close();
+    }
+
+    private static void processUserInput(String userInput, UserAuthLevel authLevel) {
+        switch (authLevel) {
+            case Client:
+                
+                break;
+            case Instructor:
+                
+                break;
+            case Admin:
+                
+                break;
+            default:
+                break;
+        }
     }
 }
