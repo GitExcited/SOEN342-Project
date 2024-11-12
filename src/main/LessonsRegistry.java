3d2ecc69e4ace11eb6a9946554ac7bd0d78901b6
David R <99555747+GitExcited@users.noreply.github.com>
2024-11-12 18:25:13 -0500

    Implemented concurrency for LessonsRegistry. Refers to #2

--- src/main/LessonsRegistry.java
+++ src/main/LessonsRegistry.java
@@ -3,6 +3,7 @@ package main;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import tdg.LessonTDG;
 
@@ -10,7 +11,7 @@ public class LessonsRegistry {
     private List<Lesson> lessonsCollection= new ArrayList<>();;
     private LessonTDG lessonTDG;
 
-    // Constructor
+    //* Constructor
     public LessonsRegistry() {
         try {
             this.lessonTDG = new LessonTDG();
@@ -20,56 +21,38 @@ public class LessonsRegistry {
         } 
     }
 
+    //* CREATE, UPDATE and DELETE Operations
+
     public void createLesson(Lesson lesson) {
+        //? Writer operates in self-exclusion
+        ReentrantReadWriteLock.WriteLock writeLock = DatabaseLock.lock.writeLock();
+        writeLock.lock();
+
         lessonsCollection.add(lesson);
         try {
             lessonTDG.insert(lesson.toParams());
         } catch (Exception e) {
             e.printStackTrace();
+        }finally {
+            //? Unlock
+            writeLock.unlock();
         }
     }
-
-    public boolean checkTimeCollision(Lesson lesson) {
-        for (Lesson l: lessonsCollection){
-            if( l.getLocation().equals(lesson.getLocation()) 
-                &&
-                l.getTimeSlot().collides(lesson.getTimeSlot())){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public String getAllLessonsDescriptions() {
-        StringBuilder sb = new StringBuilder();
-        for (Lesson l : lessonsCollection) {
-            //sb.append("LESSON NUMBER ").append(l.getID()).append("\n");
-            sb.append(l.toString()).append("\n");
-        }
-        return sb.toString();
-    }
-
-    public Lesson getLessonById(String id){
-        Lesson lesson = null;
-        for (Lesson lesson2 : lessonsCollection) {
-            if(lesson2.getID().trim().equals(id.trim())){
-                lesson = lesson2;
-                break;
-            }
-        }
-        return lesson;
-    }
-
     public void removeLesson(Lesson lesson) {
-        lessonsCollection.remove(lesson);
+        //? Writer operates in self-exclusion
+        ReentrantReadWriteLock.WriteLock writeLock = DatabaseLock.lock.writeLock();
+        writeLock.lock();
+
         try {
+            lessonsCollection.remove(lesson);
             lessonTDG.delete(lesson.getID());
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally{
+            //? Unlock
+            writeLock.unlock();
         }
     }
-
-
 
     /***
      * Updates the lesson to match the parameters of updatedLesson while keeping the same ID 
@@ -77,16 +60,84 @@ public class LessonsRegistry {
      * @param updatedLesson the lesson object with the updated values, possibly having some values not updated
      */
     public void updateLesson( int lessonId, Lesson updatedLesson) {
-
-        Lesson oldLesson = lessonsCollection.get(lessonId);
-        //! WHEN updating a lesson, we keep the same id for foreign key purposes
-        updatedLesson.setID(oldLesson.getID());
-        lessonsCollection.set(lessonId, updatedLesson);
-                
+        //? Writer operates in self-exclusion
+        ReentrantReadWriteLock.WriteLock writeLock = DatabaseLock.lock.writeLock();
+        writeLock.lock();
+ 
         try {
+            Lesson oldLesson = lessonsCollection.get(lessonId);
+            //! WHEN updating a lesson, we keep the same id for foreign key purposes
+            updatedLesson.setID(oldLesson.getID());
+            lessonsCollection.set(lessonId, updatedLesson);
             lessonTDG.update(updatedLesson.toParams());
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally{
+            //? Unlock
+            writeLock.unlock();
         }
     }
+
+    //* READ Operations */
+
+    public boolean checkTimeCollision(Lesson lesson) {
+        //? Writer operates in self-exclusion
+        ReentrantReadWriteLock.WriteLock writeLock = DatabaseLock.lock.writeLock();
+        writeLock.lock();
+
+        try{
+            for (Lesson l: lessonsCollection){
+                if( l.getLocation().equals(lesson.getLocation()) 
+                    &&
+                    l.getTimeSlot().collides(lesson.getTimeSlot())){
+                    return true;
+                }
+            }
+            return false;
+
+        }finally{
+            //? Unlock
+            writeLock.unlock();
+        }
+    }
+
+    public String getAllLessonsDescriptions() {
+        //? Writer operates in self-exclusion
+        ReentrantReadWriteLock.WriteLock writeLock = DatabaseLock.lock.writeLock();
+        writeLock.lock();
+
+        try{
+            StringBuilder sb = new StringBuilder();
+        for (Lesson l : lessonsCollection) {
+            //sb.append("LESSON NUMBER ").append(l.getID()).append("\n");
+            sb.append(l.toString()).append("\n");
+        }
+        return sb.toString();
+
+        }finally{
+            //? Unlock
+            writeLock.unlock();
+        }
+    }
+
+    public Lesson getLessonById(String id){
+        //? Writer operates in self-exclusion
+        ReentrantReadWriteLock.WriteLock writeLock = DatabaseLock.lock.writeLock();
+        writeLock.lock();
+        try{
+            Lesson lesson = null;
+            for (Lesson lesson2 : lessonsCollection) {
+                if(lesson2.getID().trim().equals(id.trim())){
+                    lesson = lesson2;
+                    break;
+                }
+            }
+            return lesson;
+        }finally{
+            //? Unlock
+            writeLock.unlock();
+        }
+    }
+
+
 }
